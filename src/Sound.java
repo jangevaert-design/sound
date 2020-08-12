e28a51a9c0733fcf3021a580db95b1aad261e6bd
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-12 12:35:27 -0600

    Initial commit

---
+++ src/Sound.java
@@ -0,0 +1,9 @@
+public class Sound {
+
+  public static void main(String[] args) {
+    double distance;
+    distance = 7.2 * 1100;
+    System.out.println("The lightning is " + distance + " feet away.");
+  }
+
+}
